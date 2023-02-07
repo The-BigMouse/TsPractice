2e7cc5788f83fbd67e104b1a15581702f8fc7816
ritik-peoplelink <ritik@peoplelinkvc.com>
2023-02-07 14:09:57 +0530

    updated

--- scratch1.ts
+++ scratch1.ts
@@ -18,3 +18,49 @@ let lastname  = "ritik";
 
 let value: any = true;
 value = 'ritik'; //? No Error as it can be any type
+
+
+//!  Types : 1.Static  2.Generics 3.Decorators :::::::::::::::::::::
+/*  Static   :::::::::::::
+        1. Built-In                      2. User-Defined
+*/
+
+// * static types mean "at compile time" or "without running a program." In a statically typed language, variables, parameters, and objects have types that the compiler knows at compile time.
+
+//! Built In Types : 
+//? 1.Number 2.Void 3String 4.Null 5.Boolean 
+
+
+//!  Number
+let first: number = 12.0;             // number   
+let second: number = 0x37CF;          // hexadecimal  
+let third: number = 0o377 ;           // octal  
+let fourth: number = 0b111001;        // binary   
+
+//! String
+
+let empName: string = "Rohan";   
+let empDept: string = "IT";   
+  
+// Before-ES6  
+let output1: string = empName + " works in the " + empDept + " department.";   
+  
+// After-ES6  
+let output2: string = `${empName} works in the ${empDept} department.`;   
+  
+console.log(output1);//Rohan works in the IT department.   
+console.log(output2);//Rohan works in the IT department.  
+
+// ? Boolean
+
+let isDone : boolean = false;
+
+//* Void : A void is a return type of the functions which do not return any type of value. It is used where no data type is available. A variable of type void is not useful because we can only assign undefined or null to them. An undefined data type denotes uninitialized variable, whereas null represents a variable whose value is undefined.
+
+let unusedValue : void = undefined;// if you try to assign value it will throw error 
+
+function greet() : void {
+    alert("Welcome User:::::::::::");
+}
+
+// ? Null : 
